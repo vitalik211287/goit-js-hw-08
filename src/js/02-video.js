c92bcab093451d651afe40e22e151af3a5536ad7
Vitalii Poliakov <vitalik211287@gmail.com>
2022-09-03 12:08:43 +0300

    completed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const updateTime = ({ seconds }) => {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
