3252626f1597a2d4e0f469e5784679c199324d43
Vitalii Poliakov <vitalik211287@gmail.com>
2022-09-03 13:32:17 +0300

    completed task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const emailInput = formEl.querySelector('[type="email"]');
+const messageInput = formEl.querySelector('[name="message"]');
+
+const savedInputData = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedInputData) {
+  const parsedObject = JSON.parse(savedInputData);
+  emailInput.value = parsedObject.email;
+  messageInput.value = parsedObject.message;
+}
+const updateStorage = () => {
+  const inputData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  const stringObj = JSON.stringify(inputData);
+  localStorage.setItem(LOCAL_STORAGE_KEY, stringObj);
+};
+
+const submitHandler = event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+};
+
+formEl.addEventListener('input', throttle(updateStorage, 500));
+formEl.addEventListener('submit', submitHandler);
