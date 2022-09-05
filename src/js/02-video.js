01577d9a850ca1ee53abea9836cb707c38d531a3
vitalik poliakov <vitalik211287@gmail.com>
2022-09-05 21:36:25 +0300

    rename goit-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,6 @@
-import throttle from 'lodash.throttle';
+
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
